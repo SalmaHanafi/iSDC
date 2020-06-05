98cd60a33ea9749a6b45426df0e5ace232e645fa
Salma Hanafi <shanafi2017@gmail.com>
2020-06-05 09:40:26 -0400

    Using isLeap

--- daysBetweenDates.py
+++ daysBetweenDates.py
@@ -1,12 +1,25 @@
+def isLeap(year):
+        if year % 4 != 0: 
+                return False
+        if year % 100 != 0:
+                return True
+        if year % 400 != 0:
+                return False
+        return True
+ 
 def nextDay(year, month, day):
-    """Simple version: assume every month has 30 days"""
-    if day < 30:
+    if isLeap(year):
+        months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30,31]
+    else:
+        months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30,31]
+    if day < months[month-1]:
         return year, month, day + 1
     else:
         if month == 12:
             return year + 1, 1, 1
         else:
             return year, month + 1, 1
+            
 def date1BeforeDate2(year1, month1, day1, year2, month2, day2):
     if year1 < year2:
         return True
@@ -15,18 +28,19 @@ def date1BeforeDate2(year1, month1, day1, year2, month2, day2):
     elif year1 == year2 and month1 == month2 and day1 < day2:
         return True
     return False
+
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """
+    Calculates the number of days between two dates.
+    """
     
-    """Returns the number of days between year1/month1/day1
-       and year2/month2/day2. Assumes inputs are valid dates
-       in Gregorian calendar, and the first date is not after
-       the second."""
+    assert date1BeforeDate2(year1, month1, day1, year2, month2, day2)
     date = year1, month1, day1
     condition = date1BeforeDate2(date[0], date[1], date[2], year2, month2, day2)
-    s =0
+    days = 0
+    
     while(condition):
-        s = s + 1
+        days = days + 1
         date = nextDay(date[0], date[1], date[2]) 
         condition = date1BeforeDate2(date[0], date[1], date[2], year2, month2, day2)
-
-    return s
+    return days
